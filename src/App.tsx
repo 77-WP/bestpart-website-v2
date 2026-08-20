fda3c2413c56556b7f3cbf582090928db5b1289e
77-WP <wiphu.phuthisabodi@gmail.com>
2026-08-20 15:48:03 +0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-background flex items-center justify-center">
+      <h1 className="text-4xl font-bold">BestPart Website v2</h1>
+    </div>
+  )
+}
+
+export default App
